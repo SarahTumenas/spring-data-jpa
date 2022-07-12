228b26761224a7d8612bf2fa8c83b36fb8dd4664
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-12 17:30:00 -0300

    Operações Crud - Atualizando o Registro

--- src/main/java/br/com/alura/spring/data/orm/Cargo.java
+++ src/main/java/br/com/alura/spring/data/orm/Cargo.java
@@ -8,15 +8,15 @@ public class Cargo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String descricao;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 @Service
 public class CrudCargoService {
 
+    private Boolean system = true;
     private final CargoRepository cargoRepository;
 
     public CrudCargoService(CargoRepository cargoRepository) {
@@ -16,7 +17,26 @@ public class CrudCargoService {
     }
 
     public void inicial(Scanner scanner) {
-        salvar(scanner);
+        while (system) {
+            System.out.println("Qual ação  de cargo você quer executar");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Atualizar");
+
+            int action = scanner.nextInt();
+
+            switch (action) {
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                default:
+                    system = false;
+                    break;
+            }
+        }
 
     }
 
@@ -30,6 +50,20 @@ public class CrudCargoService {
 
     }
 
+    private void atualizar(Scanner scanner){
+        System.out.println("Digite o id do cargo");
+        Long id = scanner.nextLong();
+
+        System.out.println("Digite a descrição do cargo");
+        String descricao = scanner.next();
+
+        Cargo cargo = cargoRepository.findById(id).get();
+
+        cargo.setDescricao(descricao);
+        cargoRepository.save(cargo);
+        System.out.println("Cargo atualizado com sucesso");
+
+    }
 
 
 }
