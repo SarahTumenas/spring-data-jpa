6561cac5d008e817b71688a8565d5efb672e496f
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-13 16:47:23 -0300

    Estrutura da Specification

---
+++ src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -0,0 +1,26 @@
+package br.com.alura.spring.data.service;
+
+import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.repository.FuncionarioRepository;
+import br.com.alura.spring.data.specification.SpecificationFuncionario;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class RelatorioFuncionarioDinamico {
+
+    private final FuncionarioRepository funcionarioRepository;
+
+    public RelatorioFuncionarioDinamico(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
+    public void inicial(Scanner scanner){
+        System.out.println("Digite o nome do funcionário:");
+        String nome = scanner.nextLine();
+
+        List<Funcionario> funcionarios = funcionarioRepository
+                .findAll(Specification.where(SpecificationFuncionario.nome(nome)));
+    }
+}
